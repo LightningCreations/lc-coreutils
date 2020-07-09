61f739313dbbea4fe0006ca2f191ec3f01ecf6c8
Connor Horman <chorman64@gmail.com>
2020-07-09 09:43:05 -0400

    Add nohup to lc-coreutils

--- hashfile/src/main.cpp
+++ hashfile/src/main.cpp
@@ -19,6 +19,8 @@
 #include <algorithm>
 
 
+#include <version.h>
+
 
 struct KnownHashFn{
     const char* name;
@@ -149,8 +151,8 @@ int main(int argc,char** argv){
                     else if(strcmp(opt,"warn")==0)
                         err_mode = FormatErrorMode::Warn;
                     else if(strcmp(opt,"version")==0){
-                        std::cout << "Lightning Creations " << argv[0] << " version 0.1" << std::endl
-                            << "Part of lc-coreutils, included in lcnix" << std::endl
+                        std::cout << "Lightning Creations " << argv[0] << std::endl
+                            << "Part of lc-coreutils version: " LCNIX_COREUTILS_VERSION ", included in lcnix" << std::endl
                             << "This program is a free software, distributed under the Terms of the GNU General Public License v3, or (at your option) any later version" << std::endl
                             << "This program is provided on an AS IS Basis, without any warranty of any kind, any implied warranties which may otherwise apply" << std::endl
                             << "If you have not recieved a copy of the GNU General Public License along with this program, you may view it on the GNU Website" << std::endl
---
+++ nohup/src.c
@@ -0,0 +1,75 @@
+//
+// Created by chorm on 2020-07-09.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+#include <version.h>
+
+_Noreturn void print_help(int code,const char* prg_name){
+    static const char HELP[] = {
+            "Usage: %s COMMAND [ARGS]\n"
+            "%s OPTION\n"
+            "Runs the given command ignoring hangup requests\n"
+            "Accepted Options:\n"
+            "\t--help: Prints this help and exits\n"
+            "\t--version: Prints version information an exits\n"
+    };
+
+    printf(HELP,prg_name,prg_name);
+    exit(code);
+}
+
+int main(int argc,char** argv){
+    if(argc<1)
+        print_help(1,argv[0]);
+    else if(*argv[1]=='-'){
+        const char* opt = argv[1]+1;
+        if(*opt++=='-'){
+            if(strcmp(opt,"help")==0)
+                print_help(0,argv[0]);
+            else if(strcmp(opt,"version")==0){
+                static const char VERSION[] = {
+                        "Lightning Creations %s\n"
+                        "Part of lc-coreutils " LCNIX_COREUTILS_VERSION " (part of lcnix)\n"
+                        "This program is a free software, released under the terms of the GNU GPLv3+\n"
+                        "This program is provided on an AS-IS basis, without any warranty\n"
+                };
+                printf(VERSION,argv[0]);
+                return 0;
+            }
+        }
+        print_help(1,argv[0]);
+    }else{
+        argv++;
+        // Redirect input and outputs to files when they are otherwise attached to a terminal
+        if(isatty(STDIN_FILENO)){
+            puts("Ignoring input");
+            int fd = open("/dev/null",O_RDONLY);
+            dup2(fd,STDIN_FILENO);
+            close(fd); // the handle is currently available in stdin, we don't need the original any more
+        }
+        if(isatty(STDOUT_FILENO)){
+            puts("Redirecting stdout to nohup.out");
+            int fd = open("nohup.out",O_WRONLY|O_CREAT,0666); // Set default mode for nohup.out, let umask set correct permissions
+            dup2(fd,STDOUT_FILENO);
+            close(fd);
+        }
+        if(isatty(STDERR_FILENO))
+            dup2(STDOUT_FILENO,STDERR_FILENO);
+        sigset_t blocked;
+        sigemptyset(&blocked);
+        sigaddset(&blocked,SIGHUP);
+        sigprocmask(SIG_BLOCK,&blocked,NULL);
+        char* prg = argv[0];
+        argv++;
+        execvp(prg,argv);
+        perror("Could not execute program");
+        return 1; // If execvp returns, an error has occured, so return 1;
+    }
+}
