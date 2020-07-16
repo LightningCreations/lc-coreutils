3803b17b865fc77aa58902ca0235e19dd609aec3
Connor Horman <chorman64@gmail.com>
2020-07-16 12:29:45 -0400

    Fix `echo --help` missing a newline

--- echo/src/echo.c
+++ echo/src/echo.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
     static const char *HELP =
             "Usage: %s [OPTION] [STRING]\n" \
             "Echos the parameters to standard output\n" \
-            "\t-n: Omits trailing newline" \
+            "\t-n: Omits trailing newline\n" \
             "\t--version: Prints version information and exits\n" \
             "\t--help: Prints this message and exits\n";
 
